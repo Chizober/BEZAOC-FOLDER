b27f3b42a183d8402d07fd29bab9dee9b1674656
Egbujie Chizoba <egbujie.chizoba@gmail.com>
2022-09-24 22:39:16 +0100

    Add files via upload

---
+++ LeapYear/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+ namespace LeapYear
+ {
+ class FindLeapYear
+{
+  public  static void Main(string[] args)
+  {
+    int currentYear = 2021;
+    int  total = 20;
+    int count = 0;
+   while (count < total)
+   {
+   currentYear = currentYear += 1;
+     if((currentYear % 4 == 0) && (currentYear  % 100 != 0) || (currentYear % 400 == 0))
+     {
+      count +=1;
+     Console.WriteLine($"{currentYear} is a leap");
+     };
+  
+   }
+  }
+}};
